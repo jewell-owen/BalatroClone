24adba88339afb1c682ee573bffe2f816715b68e
jewell-owen <jewell.owen@yahoo.com>
2024-09-21 16:47:29 -0500

    Started play activity

---
+++ AndroidExample/app/src/main/java/com/example/androidexample/PlayActivity.java
@@ -0,0 +1,21 @@
+package com.example.androidexample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlayActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /* initialize UI elements */
+
+    }
+
+}
